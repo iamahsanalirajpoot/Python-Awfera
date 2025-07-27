0e2e113d4821959257c8861fa04945224a36ca37
Ahsan Ali Rajpoot <profile.ahsanalirajpoot@gmail.com>
2025-07-27 23:59:17 +0500

    Update

--- Code_Notebooks/#22 text_processing_api.py
+++ Code_Notebooks/#22 text_processing_api.py
@@ -47,4 +47,4 @@ def process_text(request: TextRequest):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("text_processing:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("#22 text_processing_api:app", host="127.0.0.1", port=8000, reload=True)
